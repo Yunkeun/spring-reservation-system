9ccfa77bf91f6bed397a2781fbc07687c742357e
Yunkeun <yunx2@naver.com>
2022-02-19 17:57:55 +0900

    Initial commit

---
+++ src/main/java/com/yoonveloping/reservationsystem/SpringReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.reservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoonveloping/reservationsystem/SpringReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
